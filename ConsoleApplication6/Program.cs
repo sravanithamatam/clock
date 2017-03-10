dfecec5bee58d1c0034d493a0e52da01d1f9572a
Sravani Reddy <sravani.thamatam@aveva.com>
2017-03-10 17:51:49 +0530

    added changes

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApplication6
 {
@@ -11,12 +12,15 @@ namespace ConsoleApplication6
         static void Main(string[] args)
         {
             Console.WriteLine("Enter The time in HH:MM:SS Format");
+            
             string s = Console.ReadLine();
+            
             int hours=0;
             int minutes=0;
             int seconds=0;
             int count=0;
             int ? restrict=null;
+            bool result = true;
             for(int i=0;i<s.Length;i++)
             {
                 string x;
@@ -24,24 +28,31 @@ namespace ConsoleApplication6
                 {
                     x = s[i].ToString() + s[i + 1].ToString();
                     restrict = i + 1;
-                    if(count==0)
-                    hours = int.Parse(x);
-                    if(count==1)
-                        minutes = int.Parse(x);
-                    if (count == 2)
-                        seconds = int.Parse(x);
+                    if (count == 0)
+                        try
+                        {
+                            hours = int.Parse(x);
+                            if (count == 1)
+                                minutes = int.Parse(x);
+                            if (count == 2)
+                                seconds = int.Parse(x);
+                        }
+                        catch { result = false;  Console.WriteLine("Enter Correct Format as Mentioned Above"); break; }
 
                 }
                 else if(s[i]==':') { count++; }
                 
 
             }
-            double hourAngle = (360 / 12)*hours + 0.5*(seconds/60+minutes);
-            double minutAngle = (6 * minutes + 1 / 60 * 6 * seconds);
-            double secondsAngle = 360 / 60*seconds;
-            double AngleBetweenHourSecond =Math.Abs(hourAngle - secondsAngle);
+            if (result == true)
+            {
+                double hourAngle = (360 / 12) * hours + 0.5 * (seconds / 60 + minutes);
+                double minutAngle = (6 * minutes + 1 / 60 * 6 * seconds);
+                double secondsAngle = 360 / 60 * seconds;
+                double AngleBetweenHourSecond = Math.Abs(hourAngle - secondsAngle);
 
-            Console.WriteLine("Angle Between Hour hand and Second hand is "+AngleBetweenHourSecond);
+                Console.WriteLine("Angle Between Hour hand and Second hand is " + AngleBetweenHourSecond);
+            }
         }
     }
 }
